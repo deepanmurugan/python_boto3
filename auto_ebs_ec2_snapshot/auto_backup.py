a5d89ec2402485e4b49c04f08378195b986f9a5f
EC2 Default User <ec2-user@ip-10-1-0-193.us-east-2.compute.internal>
2020-01-20 17:52:11 +0000

    Lambda function to Automatically take ebs backup everyday

--- auto_ebs_ec2_snapshot/auto_backup.py
+++ auto_ebs_ec2_snapshot/auto_backup.py
@@ -1,5 +1,7 @@
 # This lambda function is used to create ec2 snapshot daily for 
 # all the EBS volume which is currently in-use status
+# Ignore if Autobackup tag is set to No for any ebs volume
+# By default Autobackup tag is set to Yes
 
 import boto3
 def lambda_handler(event, context): 
@@ -9,22 +11,26 @@ def lambda_handler(event, context):
     for region in regions:
         print ("Checking region ", region['RegionName'])
         reg=region['RegionName']
-
         ec2 = boto3.client('ec2', region_name=reg)
-
         result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}])
-        print(result)
-
+        backup = 'Yes'
+        
         for volume in result['Volumes']:
-            print("Backing up ", volume['VolumeId'], volume['AvailabilityZone'])
-            result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda function')
-            ec2resource = boto3.resource('ec2', region_name=reg)
-            snapshot = ec2resource.Snapshot(result['SnapshotId'])
+            for tag in volume['Tags']:
+                if tag['Key'] == 'Autobackup':
+                    backup = tag.get('Value')
+            if backup == 'No':
+                break
 
-            volumename = 'N/A'
-            if 'Tags' in volume:
-                for tags in volume['Tags']:
-                    if tags["Key"] == 'Name':
-                        volumename = tags["Value"]
+        print("Backing up ", volume['VolumeId'], volume['AvailabilityZone'])
+        result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda function')
+        ec2resource = boto3.resource('ec2', region_name=reg)
+        snapshot = ec2resource.Snapshot(result['SnapshotId'])
 
-            snapshot.create_tags(Tags=[{'Key': 'Name','Value': volumename}])
+        volumename = 'N/A'
+        if 'Tags' in volume:
+            for tags in volume['Tags']:
+                if tags["Key"] == 'Name':
+                    volumename = tags["Value"]
+
+        snapshot.create_tags(Tags=[{'Key': 'Name','Value': volumename}])
